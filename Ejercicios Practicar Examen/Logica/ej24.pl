c3deeba10b507f8bd7bc0793f1da0d835b6fd0eb
Pedro Varona <pedro.varonaparra@usp.ceu.es>
2026-01-10 19:20:12 +0100

    Anotaciones tests tema 4

---
+++ Ejercicios Practicar Examen/Logica/ej24.pl
@@ -0,0 +1,13 @@
+pelicula(Inception, Sci-Fi, Nolan, 2004, 10).
+pelicula(Torrente, Comedia, Segura, 2013, 9).
+pelicula(LaLaLand, Romantica, Justin, 2016, 8).
+pelicula(BadBoys, Accion, Smith, 2003, 7).
+
+preferenciapelicula(Ana, Inception).
+preferenciagenero(Franco, Accion).
+preferenciapelicula(Arnaiz, Torrente).
+preferenciagenero(Javi, Romantica).
+
+peliculavista(Ana, Inception).
+peliculavista(Arnaiz, Torrente).
+
